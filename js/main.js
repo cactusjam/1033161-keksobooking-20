85bb0344b2b000cd31ef77f7ed3557e9fb5972a1
Evgenia Alfimova <alfimova.evg@gmail.com>
2020-06-05 16:54:50 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -16,8 +16,8 @@ var advertAttributes = {
     'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
   ],
   COUNT: 8,
-  GAP_PIN_Y = 70,
-  GAP_PIN_X = 50
+  GAP_PIN_Y: 70,
+  GAP_PIN_X: 50
 };
 
 var offsetWidth = document.querySelector('.map__pins').offsetWidth;
@@ -82,8 +82,8 @@ var pin = document.querySelector('#pin').content.querySelector('.map__pin');
 
 function createPin(adv) {
   var mapPin = pin.cloneNode(true);
-  mapPin.style.left = adv.location.x - GAP_PIN_X + 'px';
-  mapPin.style.top = adv.location.y - GAP_PIN_Y + 'px';
+  mapPin.style.left = adv.location.x - advertAttributes.GAP_PIN_X + 'px';
+  mapPin.style.top = adv.location.y - advertAttributes.GAP_PIN_Y + 'px';
   mapPin.querySelector('img').alt = adv.offer.title;
   mapPin.querySelector('img').src = adv.author.avatar;
   return mapPin;
