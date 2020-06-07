69620b485ece39d6b1ec61d6465c7ae133ba46da
Evgenia Alfimova <alfimova.evg@gmail.com>
2020-06-07 22:09:35 +0300

    карточка объявления wip

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 var pinsContainer = document.querySelector('.map__pins');
+var map = document.querySelector('.map');
 var COUNT = 8;
 var advertAttributes = {
   TYPE_OF_RESIDENCE: ['palace', 'flat', 'house', 'bungalo'],
@@ -46,6 +47,13 @@ var guestNumber = {
   MAX_GUEST: 6
 };
 
+var placeType = {
+  FLAT: 'Квартира',
+  BUNGALO: 'Бунгало',
+  HOUSE: 'Дом',
+  PALACE: 'Дворец'
+};
+
 function createAdvert(index) {
   var yLocation = getRandomNumber(mapSize.MAP_Y_MIN, mapSize.MAP_Y_MAX);
   var xLocation = getRandomNumber(mapSize.MAP_X_MIN, mapSize.MAP_X_MAX);
@@ -63,9 +71,9 @@ function createAdvert(index) {
       'guests': getRandomNumber(guestNumber.MIN_GUEST, guestNumber.MAX_GUEST),
       'checkin': getRandomElement(advertAttributes.CHEKING_TIME),
       'checkout': getRandomElement(advertAttributes.CHEKING_TIME),
-      'features': getRandomElement(advertAttributes.FEATURES),
+      'features': getRandomArrayLength(advertAttributes.FEATURES),
       'description': 'Описание объявления ' + (index + 1),
-      'photos': getRandomElement(advertAttributes.PHOTOS),
+      'photos': getRandomArrayLength(advertAttributes.PHOTOS),
     },
     'location': {
       'x': xLocation,
@@ -73,6 +81,19 @@ function createAdvert(index) {
     }
   };
   return advert;
+}
+
+function getRandomArrayLength(array) {
+  var newArrayLength = getRandomNumber(1, array.length);
+  var newArray = [];
+
+  while (newArrayLength > newArray.length) {
+    var randomElement = getRandomElement(array);
+    if (newArray.includes(randomElement) === false) {
+      newArray.push(randomElement);
+    }
+  }
+  return newArray;
 }
 
 function getAvatarPath(index) {
@@ -96,10 +117,10 @@ function createAdvertCollection() {
   }
   return adverts;
 }
-createAdvertCollection();
+var advertCollection = createAdvertCollection();
 
 
-document.querySelector('.map').classList.remove('map--faded');
+map.classList.remove('map--faded');
 
 var pin = document.querySelector('#pin').content.querySelector('.map__pin');
 
@@ -122,3 +143,44 @@ function renderMapPins() {
   pinsContainer.appendChild(fragment);
 }
 renderMapPins();
+
+
+var firstAdvert = advertCollection[0];
+var cardTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.map__card');
+
+var renderAdvertCard = function (advertData) {
+  var cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = advertData.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = advertData.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = advertData.offer.price + ' ₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = placeType[advertData.offer.type.toUpperCase()];
+  cardElement.querySelector('.popup__text--capacity').textContent = advertData.offer.rooms + ' комнаты для ' + advertData.offer.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + advertData.offer.checkin + ', выезд до ' + advertData.offer.checkout;
+  filterFeatures(cardElement, advertData);
+  cardElement.querySelector('.popup__description').textContent = advertData.offer.description;
+  return cardElement;
+};
+
+function filterFeatures(cardElement, advertData) {
+  var cardFeatureCollection = cardElement.querySelectorAll('.popup__feature');
+  for (var i = 0; i < cardFeatureCollection.length; i++) {
+    var liElement = cardFeatureCollection[i];
+    var featurePresent = false;
+
+    for (var advI = 0; advI < advertData.offer.features.length; advI++) {
+      if (liElement.className.includes(advertData.offer.features[advI]) === true) {
+        featurePresent = true;
+      }
+    }
+
+    if (featurePresent === false) {
+      liElement.remove();
+    }
+  }
+}
+
+console.log(renderAdvertCard(firstAdvert));
+renderAdvertCard(firstAdvert);
+
